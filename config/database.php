42519d892b49acc1f499efb272418784ff0a32a6
Hanbiike <hanbiike.corp@gmail.com>
2025-12-17 12:00:26 +0600

    bug fix

--- config/database.php
+++ config/database.php
@@ -7,12 +7,12 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'AEA_DB');
 define('DB_USER', 'root');
-define('DB_PASS', 'Aizada2025!'); // Измените на ваш пароль MAMP
+define('DB_PASS', 'root'); // Измените на ваш пароль MAMP
 define('DB_CHARSET', 'utf8mb4');
 
 // Настройки приложения
 define('APP_NAME', 'AEA System');
-define('APP_URL', 'http://localhost/phpodevler/employee-task-rewarding-system');
+define('APP_URL', 'http://localhost:8888/employee-rewarding-system');
 define('SESSION_TIMEOUT', 3600); // 1 час
 
 // Настройки безопасности
